8a10a88f392c0bfa150066139cfddca49aed7310
Xinrui Yan <ansonyxr@gmail.com>
2021-06-14 23:14:22 -0700

    add spring mvc config

--- src/main/java/com/egen/AppConfig.java
+++ src/main/java/com/egen/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -13,4 +14,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class AppConfig extends WebMvcConfigurationSupport {
 	//TODO: override addCorsMappings Method to allow cross origin request to API
 
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
