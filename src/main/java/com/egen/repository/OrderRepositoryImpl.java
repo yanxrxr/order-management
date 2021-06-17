4c3372e7b1da9d92a49857ffa31490459b7b2d18
Xinrui Yan <ansonyxr@gmail.com>
2021-06-17 12:33:11 -0700

    add controller, service and repository

---
+++ src/main/java/com/egen/repository/OrderRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.egen.repository;
+
+import com.egen.model.Order;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.NamedQueries;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private Map<String, Order> orderMap = new HashMap<>();
+
+    @Override
+    public List<Order> getAllOrders() {
+        return new ArrayList<>(orderMap.values());
+    }
+
+    @Override
+    public Order getOrderById(String orderId) {
+        return orderMap.get(orderId);
+    }
+
+    @Override
+    public List<Order> getAllOrdersWithinInterval(ZonedDateTime startTime, ZonedDateTime endTime) {
+        return null;
+    }
+
+    @Override
+    public List<Order> top10OrdersWithHighestDollarAmountInZip(String zip) {
+        return null;
+    }
+
+    @Override
+    public Order placeOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order cancelOrder(String id) {
+        return null;
+    }
+
+    @Override
+    public Order updateOrder(String orderId, Order order) {
+        return null;
+    }
+}
---
+++ src/main/java/com/egen/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.egen.service;
+
+import com.egen.model.Order;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public interface OrderService {
+
+    List<Order> getAllOrders();
+
+    Order getOrderById(String orderId);
+
+    List<Order> getAllOrdersWithinInterval(ZonedDateTime startTime, ZonedDateTime endTime);
+
+    List<Order> top10OrdersWithHighestDollarAmountInZip(String zip);
+
+    Order placeOrder(Order order);
+
+    Order cancelOrder(String id);
+
+    Order updateOrder(String orderId, Order order);
+
+}
